1846eed6ec3d101207e957972563d5dc3200eb9e
Maheshi2Chamika <wijekoonchami@gmail.com>
2025-04-09 08:52:34 -0700

    Add files via upload

---
+++ studentdb.js
@@ -0,0 +1,8 @@
+let  students = [
+    {regno:"2021ICT01",Name:"Silwa",age:25,course:"IT",gender:"male"},
+    {regno:"2021ICT02",Name:"Nawarathna",age:24,course:"Bio",gender:"female"},
+    {regno:"2021ICT03",Name:"Ariyawansha",age:26,course:"IT",gender:"female"},
+    {regno:"2021ICT04",Name:"Wijeykoon",age:25,course:"Bio",gender:"male"},
+    {regno:"2021ICT05",Name:"Dayarathna",age:23,course:"IT",gender:"female"}
+];
+module.exports = students;
